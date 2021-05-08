9c6bbcbbd543db06f5b0544598e4cae41804426b
Lee Burton <lburton@lburton.org>
2021-05-07 17:04:55 -0700

    Updates to prereqs, generation, perms, docs, and coverage

--- Makefile
+++ Makefile
@@ -4,11 +4,6 @@ CURRENT_DATE = $(shell date -u +%F)
 
 install-deps:
 	pip3 install -r requirements.txt
-	# this has to be installed separately until 0.19 is released https://github.com/SciTools/cartopy/issues/1552
-	pip3 install cartopy==0.18
 
 generate-coverage:
-	seq 0 3 | parallel -j4 "./main.py {}"
-	mv tle_cache/starlink.txt tle_cache/starlink-${CURRENT_DATE}.txt
-	mv h3_4_cov_full.txt h3_4_cov_op_${CURRENT_DATE}.txt
-	./merge_cover.py
+	./gen_cov.sh
--- gen_cov.sh
+++ gen_cov.sh
@@ -1,5 +1,25 @@
-date
-seq 0 3 | parallel -j4 "./main.py {}"
+echo "Starting at `date -Iminutes`"
+#cleanup and prep 
+rm h3_4_*.txt
+mkdir -p archive
+
+#generate index
+./gen_h3_index.py
+
+for i in 25 35; do 
+  echo "Starting $i degrees at `date -Iminutes`"
+  seq 0 15 | parallel -j16 "./main.py {} 16 $i"
+  ./merge_cover.py
+  mv h3_4_cov_full.txt archive/h3_4_cov_op_`date -u +%F`_$i.txt
+  mv h3_4_cov_op.bin h3_4_cov_op_$i.bin
+  cp h3_4_cov_op_$i.bin archive/h3_4_cov_op_`date -u +%F`_$i.bin
+  rm h3_4_cov_*.txt
+  echo "Finished $i degrees at `date -Iminutes`"
+done
+
+#archive tle for today
 mv tle_cache/starlink.txt tle_cache/starlink-`date -u +%F`.txt
-mv h3_4_cov_full.txt h3_4_cov_op_`date -u +%F`.txt
-./merge_cover.py
+
+#cleanup
+rm h3_4_*.txt
+echo "Done at `date -Iminutes`"
